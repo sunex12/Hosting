2f6999d4367007c4d22a185daa4604a2bab059da
Andris Potapenko <andrispotapenko@gmail.com>
2023-07-27 18:08:13 +0300

    new

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const burger = document.querySelector('.header__burger');
+// const redUnderline = document.querySelector('.red-underline');
+const headerList = document.querySelectorAll('.header__list a');
 
 function burgerDisplay() {
     if(window.innerWidth <= 1300) {
         burger.style.display = 'flex';
+        redUnderline.style.display = 'none';
+        headerList.forEach(element => {
+            element.style.color = 'red';
+        });
     } else if (window.innerWidth > 1300) {
         burger.style.display = 'none';
+        redUnderline.style.display = 'inline-block';
+        headerList.forEach(element => {
+            element.style.color = 'black';
+        });
     }
 }
 
