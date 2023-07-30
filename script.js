399ff035245d7a47b7267b3ab1e894efb8fc50b2
Andris Potapenko <andrispotapenko@gmail.com>
2023-07-30 20:27:52 +0300

    new

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const display = document.querySelector('.display');
-const headerListLink = document.querySelectorAll('.header__list a');
-
-function setWidth() {
-    if (window.innerWidth <= 1300) {
-        display.style.display = 'flex';
-        headerListLink.forEach(item => {
-            item.style.color = 'white';
-        })
-    } else if (window.innerWidth > 1300) {
-        display.style.display = 'none';
-        headerListLink.forEach(item => {
-            item.style.color = 'black';
-        })
-    }
-}
-
-window.addEventListener('resize', setWidth);
-
-
 const burger = document.querySelector('.header__burger');
 const headerList = document.querySelector('.header__list');
 
